0c671901ab3360e09cde4e2470119feb4613b34a
Randal Cunanan <randal@randalvance.net>
2020-09-30 13:51:14 +0800

    Add GraphQL Yoga

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!';
+        },
+        name() {
+            return 'Randal Cunanan';
+        },
+        location() {
+            return 'Singapore';
+        },
+        bio() {
+            return 'I code stuffs!';
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+});
+
+server.start(() => {
+    console.log('Server is running at port 4000!');
+});
