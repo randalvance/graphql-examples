4318faebbebf149608f4d0670eb38427fc69c9e7
Randal Cunanan <randal@randalvance.net>
2020-10-09 17:23:07 +0800

    Guard user email

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,2 +1,13 @@
+import { getUserId } from '../auth/getUserId';
+
 export const User = {
+    email(parent, args, { request }, info) {
+        const userId = getUserId(request, false);
+        
+        if (userId && userId === parent.id) {
+            return parent.email;
+        } else {
+            return null;
+        }
+    }
 };
