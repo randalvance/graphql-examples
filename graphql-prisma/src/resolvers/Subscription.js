1f5ada50fdd1d38b748d6e4bd5689c2d8322b4eb
Randal Cunanan <randal@randalvance.net>
2020-10-09 14:56:58 +0800

    Connect subscriptions to Prisma

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,32 +1,26 @@
 export const Subscription = {
-  count: {
-    subscribe(parent, args, { pubSub }, info) {
-        let count = 0;
-
-        setInterval(() => {
-            count++;
-            pubSub.publish('count', {
-                count,
-            });
-        }, 1000);
-
-        return pubSub.asyncIterator('count');
-    },
-  },
   post: {
-    subscribe(parent, args, { pubSub }, info) {
-      return pubSub.asyncIterator('posts');
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post({
+        where: {
+          node: {
+            published: true,
+          },
+        },
+      }, info);
     }
   },
   comment: {
-      subscribe(parent, { postId }, { db, pubSub }, info) {
-        const post = db.posts.find(p => p.id === postId && p.published);
-
-        if (!post) {
-            throw new Error(`Published post with ID ${postId} does not exist!`);
-        }
-
-        return pubSub.asyncIterator(`comments of post ${postId}`);
+      subscribe(parent, { postId }, { prisma }, info) {
+        return prisma.subscription.comment({
+          where: {
+            node: {
+              post: {
+                id: postId,
+              }
+            },
+          },
+        }, info);
       }
   }
 };
