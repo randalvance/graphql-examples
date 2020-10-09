352d0d84bceb2ca3c6030591c001777a9f70e509
Randal Cunanan <randal@randalvance.net>
2020-10-09 14:36:10 +0800

    Cleanup

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,5 +1,3 @@
-import { v4 as uuid } from "uuid";
-
 export const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     const emailTaken = await prisma.exists.User({ email: args.data.email });
